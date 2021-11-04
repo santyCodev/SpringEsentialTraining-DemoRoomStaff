1b8a8f2c4e898d174341159739efc5381638e527
Santiago <santi.codev@gmail.com>
2021-11-04 15:35:59 +0100

    Creacion y configuracion de profiles

---
+++ src/main/java/com/santicodev/springessentialtraining/SpringessentialtrainingApplication.java
@@ -0,0 +1,13 @@
+package com.santicodev.springessentialtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringessentialtrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringessentialtrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santicodev/springessentialtraining/SpringessentialtrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santicodev.springessentialtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringessentialtrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
