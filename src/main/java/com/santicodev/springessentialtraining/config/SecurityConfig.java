36dc83bde4244c7625cfae3698ea84b10466a741
Santiago <santi.codev@gmail.com>
2021-11-10 12:56:11 +0100

    Implementacion de spring data rest

--- src/main/java/com/santicodev/springessentialtraining/config/SecurityConfig.java
+++ src/main/java/com/santicodev/springessentialtraining/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/api/*").permitAll()
+        http.authorizeRequests().antMatchers("/", "/api/*", "/api/rest/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
